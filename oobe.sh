208619884755b2a533db0d8aef20d8c76dd5e888
gabriel <gabriel@okteto.com>
2022-03-21 12:29:22 +0100

    sam install

--- oobe.sh
+++ oobe.sh
@@ -1,9 +1,15 @@
 
-  rm -rf sam-installation
+  rm -rf aws
   curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip -d sam-installation
-  ./sam-installation/install
+  unzip awscliv2.zip
+  cd aws
+  ./install
   aws --version
+
+  curl "https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip"
+  unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+  cd sam-installation
+  ./install
   sam --version
 
     #  cd lambda/py/
